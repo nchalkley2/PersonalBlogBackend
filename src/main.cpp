f908baac8317c72e02fcd552c4653d6512b597f9
Nicholas Chalkley <ensaides@gmail.com>
2017-10-23 16:27:56 -0400

    works

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 //#include "pistache/pistache.h"
+// Argh
+#include "argh.h"
+
+// Pistache
 #include "pistache/endpoint.h"
 #include "pistache/net.h"
 
+#include <iostream>
+#include <string>
+
 using namespace Pistache;
-//using namespace Pistache::Net;
 
 class HelloHandler : public Http::Handler {
 	public:
@@ -11,12 +17,23 @@ class HelloHandler : public Http::Handler {
 	HTTP_PROTOTYPE(HelloHandler)
 
 	void onRequest(const Http::Request& request, Http::ResponseWriter response) {
+		std::cout << "request.resource(): " << request.resource() << std::endl;
 		response.send(Http::Code::Ok, "Hello, World");
 	}
 };
 
-int main() {
-	Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(9080));
+int main(int argc, char* argv[]) 
+{
+	argh::parser cmdl({"-p", "--port"});
+	cmdl.parse(argc, argv);
+
+	std::cout << cmdl("port").str() << std::endl;
+	
+
+	int port;
+	cmdl({"-p", "--port"}, 80) >> port;
+
+	Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(port));
 
     auto opts = Http::Endpoint::options().threads(1);
     Http::Endpoint server(addr);
